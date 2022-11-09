3b6eb551514a7db8a5e8666bbc3c8c4a957d56f3
Anuj <anujtanwar89@gmail.com>
2022-11-08 22:32:46 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ import { Home } from "./components/pages/Home";
 import { Edit } from "./components/pages/Edit";
 import { Navbar } from "./components/navbar/Navbar";
 
+// import { initialize_store } from './features/product';
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { useState } from 'react'
+import { db } from './firestore'
+
 function App() {
+
+  const [headphones, setHeadphones] = useState([])
+  const headphonesArray = []
+
+  const q = query(collection(db, 'headphones'))
+  onSnapshot(q, querySnapshot => {
+    querySnapshot.forEach(doc => {
+      console.log(doc.id)
+      console.log(doc.data())
+      const headphonesData = {
+        keyName: doc.id,
+        ...doc.data(),
+      }
+      headphonesArray.push(headphonesData)
+    })
+    setHeadphones(headphonesArray)
+  })
+
   return (
     <div className="App">
       <Navbar />
